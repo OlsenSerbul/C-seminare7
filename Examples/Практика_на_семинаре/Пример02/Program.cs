f3b6eec10651dc3ba06806e4c446099ec735bab7
Olga Serbul <Olsen.zika@mail.ru>
2022-11-23 16:21:50 +0300

    пример02_массив_по_формуле_m+n

---
+++ Examples/Практика_на_семинаре/Пример02/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задайте двумерный массив размера m на n,
+// каждый элент которого находится по формуле A = m+n
+// Выведите полученный массив на экран.
+
+Console.Write("Введите число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = new int[m, n];
+
+for (int i = 0; i < m; i++)
+{
+    for(int j = 0; j < n; j++)
+    {
+        array[i, j] = i + j;
+        Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine();
+}
