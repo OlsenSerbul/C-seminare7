0118a0d0713acb1866590cefc4f6f90b00d45662
Olga Serbul <Olsen.zika@mail.ru>
2022-12-01 10:30:56 +0300

    задача_63_практика на семинаре

---
+++ Examples/Семинар_9/63/Program.cs
@@ -0,0 +1,20 @@
+﻿// задайте значение N, напишите программу, которая
+// выведет все значения от N до 1.
+Console.Write("Введите N:  ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+string Znacheniya(int N)
+{
+    string rezult = "";
+    if(N == 1)
+    {
+        rezult = $"{rezult} 1 ";
+        return rezult;
+    }
+    else
+    {
+        return $"{N} , {Znacheniya(N -1 )}";
+    }
+}
+string result = Znacheniya(N);
+Console.WriteLine(result);
