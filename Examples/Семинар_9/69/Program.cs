a13d72c094168686ce49b95ff1bb451d7ba63732
Olga Serbul <Olsen.zika@mail.ru>
2022-12-01 14:22:37 +0300

    задача_69_практика на семинаре

---
+++ Examples/Семинар_9/69/Program.cs
@@ -0,0 +1,21 @@
+﻿//Напишите программу, которая на вход принимает два числа A и B.
+// и возводит число А в степень В с помощью рекурсии.
+
+Console.Write("Введите A:  ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите B:  ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+int Power(int A, int B)
+{
+    if(B == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return A * Power(A, B - 1);
+    }
+}
+Console.WriteLine(Power(A, B));
