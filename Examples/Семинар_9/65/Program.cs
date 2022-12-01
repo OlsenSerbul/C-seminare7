3d58c0f8ae13ba34a3f84d180765e2ddc247a3e2
Olga Serbul <Olsen.zika@mail.ru>
2022-12-01 13:01:10 +0300

    задача_67_практика на семинаре

---
+++ Examples/Семинар_9/65/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задайте значения M и N. Напишите программу, которая
+// выведет все натуральные числа в промежутке от М до N.
+
+string FindPromejutokNM(int M, int N)
+{
+    string rezult = " ";
+    if( M == N)
+    {
+        rezult += " ";
+        return rezult;
+    }
+    else
+    {
+        return rezult + FindPromejutokMN(M, N + 1);
+        
+    }
+}
+string FindPromejutokMN(int M, int N)
+{
+    string rezult = "";
+    if( N == M)
+    {
+        rezult += " ";
+        return rezult;
+    }
+    else
+    {
+        return rezult + FindPromejutokMN(M + 1, N);
+        
+    }
+}
+
+Console.Write("Введите M:  ");
+int M = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите N:  ");
+int N = Convert.ToInt32(Console.ReadLine());
+ if ( M > N );
+ {
+    Console.Write(FindPromejutokNM(M,N));
+ }
+ if (M == N)
+ {
+    Console.WriteLine("Промежутка нет");
+ }
+ if(M < N)
+ {
+   Console.Write(FindPromejutokMN(M,N));
+ }
+
---
+++ Examples/Семинар_9/67/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая будет принимать на вход число и возвращать егое сумму цифр
+
+Console.Write("Введите M:  ");
+int M = Convert.ToInt32(Console.ReadLine());
+
+int FindSum(int M)
+{
+    
+    if (M  < 10 )
+    {
+        return M;
+        
+    }
+    else
+    {
+        
+        return M % 10 + FindSum(M / 10 );
+    }
+}
+Console.WriteLine(FindSum(M));
