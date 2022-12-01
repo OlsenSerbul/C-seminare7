2e05c584d7f426f43ec125471bc1ea189405d642
Olga Serbul <Olsen.zika@mail.ru>
2022-12-01 19:15:23 +0300

    задача_67__практика на семинаре

---
+++ Examples/Семинар_9/64/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все 
+// натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+Console.Write("Введите N:  ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+string Znacheniya(int N)
+{
+    string rezult = "";
+    if(N == 1)
+    {
+        rezult = $"{rezult} 1 ";
+        return rezult;
+    }
+    else
+    {
+        return $"{N} , {Znacheniya(N -1 )}";
+    }
+}
+string result = Znacheniya(N);
+Console.WriteLine(result);
---
+++ Examples/Семинар_9/65/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт 
+// сумму натуральных элементов в промежутке от M до N.
+
+Console.Write("Введите начало промежутка М: ");
+int M = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите конец промежутка N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int SumNumbers(int M, int N)
+{
+if (M == 0)
+{
+    return ( N * (N + 1))/ 2;
+}
+else 
+    if (N == 0)
+    {
+        return (M *( M + 1))/ 2;
+    }
+    if (M == N)
+    {
+        return M;
+    }
+    if (M < N)
+    {
+        return N + SumNumbers(M, N -1);
+    }
+    else return N + SumNumbers(M, N + 1);
+}
+Console.WriteLine($"Сумма = {SumNumbers(M, N)}");
