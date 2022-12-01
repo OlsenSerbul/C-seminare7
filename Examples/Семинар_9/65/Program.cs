6908f4ce4ac604c54f2c43dddd39f1f70e022b92
Olga Serbul <Olsen.zika@mail.ru>
2022-12-01 14:55:28 +0300

    задача_65_практика на семинаре

--- Examples/Семинар_9/65/Program.cs
+++ Examples/Семинар_9/65/Program.cs
@@ -1,33 +1,13 @@
 ﻿// Задайте значения M и N. Напишите программу, которая
 // выведет все натуральные числа в промежутке от М до N.
 
-string FindPromejutokNM(int M, int N)
+string NumberRec(int M, int N);
 {
-    string rezult = " ";
-    if( M == N)
+    if(M <= N)
     {
-        rezult += " ";
-        return rezult;
+        return NumberRec(M, N -1) + $"{N}";
     }
-    else
-    {
-        return rezult + FindPromejutokMN(M, N + 1);
-        
-    }
-}
-string FindPromejutokMN(int M, int N)
-{
-    string rezult = "";
-    if( N == M)
-    {
-        rezult += " ";
-        return rezult;
-    }
-    else
-    {
-        return rezult + FindPromejutokMN(M + 1, N);
-        
-    }
+    else return String.Empty;
 }
 
 Console.Write("Введите M:  ");
@@ -35,16 +15,5 @@ int M = Convert.ToInt32(Console.ReadLine());
 
 Console.Write("Введите N:  ");
 int N = Convert.ToInt32(Console.ReadLine());
- if ( M > N );
- {
-    Console.Write(FindPromejutokNM(M,N));
- }
- if (M == N)
- {
-    Console.WriteLine("Промежутка нет");
- }
- if(M < N)
- {
-   Console.Write(FindPromejutokMN(M,N));
- }
+Console.WriteLine(NumberRec(M, N));
 
